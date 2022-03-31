8adeadda266b16d6a0e2417b2a211d6771e906b4
Aqb09 <aqibb.hossain@gmail.com>
2022-03-31 11:44:48 +0600

    ok

--- ageCalc.js
+++ ageCalc.js
@@ -4,7 +4,7 @@ function ageCalculate() {
      
      let today = new Date();
         let inputDate = new Date(document.getElementById("date-input").value);
-        let birthMonth, birthDate, birthYear;
+        let birthDate,birthMonth, birthYear;
 
         let birthDetails = {
             date: inputDate.getDate(),
